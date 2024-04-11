545ac5ae435afb8548ff4c36f282c32a46c71a53
bohan zhu <zbhh160@gmail.com>
2024-04-10 18:21:46 -0600

    Initial commit

---
+++ ReactJS_Django/reactjs_diango_web_app/config/settings/base.py
@@ -0,0 +1,47 @@
+# project_name/config/settings/base.py
+
+# Django settings for base configuration
+
+DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'apps.polls',  # Add your apps here
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATIC_URL = 'static/'
+
+ROOT_URLCONF = 'config.urls'
+
+SECRET_KEY = 'django-insecure-vx@olbtmcb3_f0hgd2m(!@-0au&5ukdq42^=+5%x#l7v6mf_^w'
---
+++ ReactJS_Django/reactjs_diango_web_app/config/settings/development.py
@@ -0,0 +1,7 @@
+# project_name/config/settings/development.py
+
+# Django settings for development environment
+
+from .base import *
+
+# Override base settings as needed for development
